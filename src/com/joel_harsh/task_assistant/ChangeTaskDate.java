b89da548fcb98bb731f2f7633a960c071c103068
joelmoniz <jramoniz@gmail.com>
2014-01-12 12:09:06 +0530

    Added DatePicker to change date option

--- src/com/joel_harsh/task_assistant/ChangeTaskDate.java
+++ src/com/joel_harsh/task_assistant/ChangeTaskDate.java
@@ -1,6 +1,6 @@
 package com.joel_harsh.task_assistant;
 
-import com.joel_harsh.task_assistant.R;
+import java.util.Calendar;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -9,13 +9,14 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.DatePicker;
+import android.widget.DatePicker.OnDateChangedListener;
 import android.widget.Toast;
 
-public class ChangeTaskDate extends Activity implements OnClickListener {
+public class ChangeTaskDate extends Activity implements OnClickListener, OnDateChangedListener {
 
 	Button b;
-	EditText t;
+	DatePicker dp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +25,11 @@ public class ChangeTaskDate extends Activity implements OnClickListener {
 
 		b = (Button) findViewById(R.id.button1);
 		b.setOnClickListener(this);
+		
 
-		t = (EditText) findViewById(R.id.editText1);
+		dp = (DatePicker) findViewById(R.id.datePicker1);
+		Calendar calendar = Calendar.getInstance();
+		dp.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), this);
 	}
 
 	@Override
@@ -39,13 +43,20 @@ public class ChangeTaskDate extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		String s = getIntent().getExtras().getString(StoreConst.Store);
-		String d = t.getText().toString();
+		String d = dp.getYear() + "/" 	+ (dp.getMonth() + 1) + "/" + dp.getDayOfMonth() ;
 		SQLManager.createObject(getApplicationContext()).updateDate(s, d);
 
 		Intent i2 = new Intent(this, MainActivity.class);
 		startActivity(i2);
 
 		Toast.makeText(this, "Date updated.", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onDateChanged(DatePicker view, int year, int monthOfYear,
+			int dayOfMonth) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
